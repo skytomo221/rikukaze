fb908af482f46162a230acc5246ed44f6fb74980
skytomo <tomo1kawa.fullerene@gmail.com>
2021-09-20 11:45:51 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -15,57 +15,69 @@ sleeptime = 1
 
 
 def datatime_range(begin: datetime, end: datetime) -> datetime:
-    dt = begin
-    while dt < end:
-        yield dt
-        dt = dt + timedelta(days=1)
+    length = (end - begin).days + 1
+    for n in range(length):
+        yield begin + timedelta(n)
 
 
-def markdown(content) -> str:
-    if type(content) is str:
-        return content
-    else:
-        if content.name == 'h1':
-            return f'\n# {content.get_text()}\n'
-        elif content.name == 'h2':
-            return f'\n## {content.get_text()}\n'
-        elif content.name == 'p':
-            return content.get_text()
+class Rikukaze:
+
+    def __init__(self, url):
+        self.url = url
+
+    def get(self):
+        response = requests.get(self.url)
+        response.encoding = response.apparent_encoding
+        self.soup = BeautifulSoup(response.text, 'html.parser')
+
+    @staticmethod
+    def found(soup: BeautifulSoup) -> bool:
+        return soup.body.h1.string != not_found_message
+
+    def analyze(self):
+        self.article = self.soup.body.main.article
+        self.title = self.article.header.h1.string
+        self.content = self.article.find('div', class_='content')
+
+    @staticmethod
+    def markdown(content) -> str:
+        if type(content) is str:
+            return content
         else:
-            return ''
+            if content.name == 'h1':
+                return f'\n# {content.get_text()}\n'
+            elif content.name == 'h2':
+                return f'\n## {content.get_text()}\n'
+            elif content.name == 'p':
+                return content.get_text()
+            else:
+                return ''
 
+    def to_markdown(self) -> str:
+        text = '\n'.join(map(self.markdown, self.content.contents)).strip()
+        text = f'# {self.title}\n\n{text}\n'
+        text = re.sub(r'\n\n+', '\n\n', text)
+        return text
 
-def not_found(soup: BeautifulSoup) -> bool:
-    return soup.body.h1.string == not_found_message
+    def save(self):
+        self.get()
 
+        if not self.found(self.soup):
+            print(f'{self.url} -> 404 Not Found')
+            return
+        else:
+            print(f'{self.url} -> 200 OK')
 
-def req_and_save_site(url):
-    response = requests.get(url)
-    response.encoding = response.apparent_encoding
+        self.analyze()
+        text = self.to_markdown()
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    if not_found(soup):
-        print(f'{url} -> 404 Not Found')
-        return
-    else:
-        print(f'{url} -> 200 OK')
-
-    article = soup.body.main.article
-    title = article.header.h1.string
-    content = article.find('div', class_='content')
-
-    article_text = '\n'.join(list(map(markdown, content.contents))).strip()
-    article_text = re.sub(r'\n\n+', '\n\n', article_text)
-    article_text = article_text + '\n'
-
-    with open(f'{output_dir}/{title}.md', 'w') as f:
-        f.write(article_text)
+        with open(f'{output_dir}/{self.title}.md', 'w') as f:
+            f.write(text)
 
 
 if __name__ == "__main__":
-    dtr = datatime_range(begin, end)
-    for dt in dtr:
+    for dt in datatime_range(begin, end):
         full_url = f'{base_url}{dt.strftime(date_format)}'
-        req_and_save_site(full_url)
+        rikukaze = Rikukaze(full_url)
+        rikukaze.save()
         time.sleep(sleeptime)
